e57f77ffea2967c96fb581ec6c56e63e97bb7d83
syahroni92 <m.syahroni.92@gmail.com>
2024-05-16 18:57:20 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# hilink
+# SCRIPT MODEM HILINK DAN PETUNJUK INSTALL
+# TESTED
+- ***Modem E5577***
+- ***Modem E5372***
+- ***Modem E5573***
+- ***Modem E3372***
+- ***Modem STC/MOD HUAWEI***
+
+
+Copy paste di terminal OPENWRT:
+```
+bash -c "$(wget -qO - 'https://raw.githubusercontent.com/syahroni92/hilink/master/setup.sh')"
+```
+
+# Untuk menjalankan auto ganti ip ketika tidak ada koneksi
+1. CARA 1
+
+Hapus terlebih dahulu jika stbmu 
+- B860H<br>
+  copy paste di terminal script berikut<br>
+  ```
+  rm -f /usr/bin/bled && wget -O https://raw.githubusercontent.com/syahroni92/hilink/main/bled-hgled/bled && chmod +x /usr/bin/bled && /usr/bin/bled -r 
+  ```
+- HG680P<br>
+  copy paste di terminal script berikut<br>
+  ```
+  rm -f /usr/bin/hgled && wget -O /usr/bin/hgled https://raw.githubusercontent.com/syahroni92/hilink/main/bled-hgled/hgled && chmod +x /usr/bin/hgled && /usr/bin/hgled -r
+  ```
+<br><br>
---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/ash
+# Installation script by ARYO.
+
+DIR=/usr/bin
+
+
+finish(){
+clear
+	echo ""
+    echo "INSTALL SUCCESSFULLY ;)"
+    echo ""
+    echo "Untuk Menjalankan Ketik m dan enter di terminal"
+    sleep 3
+    echo ""
+    echo "SALAM SEDULURAN"
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    	clear
+  	echo "Downloading files from repo hilink mak cling..."
+   	wget -O $DIR/m https://raw.githubusercontent.com/syahroni92/hilink/main/usr/bin/m && chmod +x $DIR/m
+ 	wget -O $DIR/hilink https://raw.githubusercontent.com/syahroni92/hilink/main/usr/bin/hilink && chmod +x $DIR/hilink
+  	wget -O $DIR/balong-nvtool https://raw.githubusercontent.com/syahroni92/hilink/main/usr/bin/balong-nvtool && chmod +x $DIR/balong-nvtool
+   	wget -O $DIR/updatehilink https://raw.githubusercontent.com/syahroni92/hilink/main/usr/bin/balong-nvtool && chmod +x $DIR/updatehilink
+    	finish
+}
+
+echo ""
+echo "Install Script code from repo aryo."
+
+while true; do
+    read -p "This will download the files into $DIR. Do you want to continue (y/n)? " yn
+    case $yn in
+        [Yy]* ) download_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer 'y' or 'n'.";;
+    esac
+done
